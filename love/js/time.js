7def9894b224cbe13512c3b73a41934f436d1bec
suzhiluoboge <150531835+suzhiluoboge@users.noreply.github.com>
2023-12-31 16:36:48 +0800

    Update time.js

--- love/js/time.js
+++ love/js/time.js
@@ -6,7 +6,7 @@ date.setUTCHours(0, 0, 0, 0);
 return date; 
 } 
 function showsectime() {
-var birthDay =NewDate("2022-11-27");
+var birthDay =NewDate("2023-12-28");
 var today=new Date();
 var timeold=today.getTime()-birthDay.getTime();
 var sectimeold=timeold/1000
@@ -17,6 +17,13 @@ var e_hrsold=(daysold-e_daysold)*-24;
 var hrsold=Math.floor(e_hrsold);
 var e_minsold=(hrsold-e_hrsold)*-60;
 var minsold=Math.floor((hrsold-e_hrsold)*-60); var seconds=Math.floor((minsold-e_minsold)*-60).toString();
-document.getElementById("showsectime").innerHTML = "我们相恋了"+daysold+"天"+hrsold+"小时"+minsold+"分"+seconds+"秒";
+
+var hand=hrsold-10;
+if (hand<0)
+{
+hand=24+hand;
+daysold=daysold-1;
+}
+document.getElementById("showsectime").innerHTML = "我们相恋了"+daysold+"天"+hand+"小时"+minsold+"分"+seconds+"秒";
 setTimeout(showsectime, 1000);
 }
